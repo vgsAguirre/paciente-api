e28ab5935f9f6d180a9b30614b45ccee56c9c4e3
Vinicius Gomes da Silva <vgs.aguirre@gmail.com>
2026-02-11 10:22:12 -0300

    refactor: removido import não utilizado do objeto PacienteController

--- src/main/java/com/gomes/pacienteapi/controller/PacienteController.java
+++ src/main/java/com/gomes/pacienteapi/controller/PacienteController.java
@@ -9,11 +9,10 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
